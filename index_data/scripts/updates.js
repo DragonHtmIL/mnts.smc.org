8399da370e73ccb9d21fd3f73375f348190c5ba0
DragonHtmIL <meirfons1@gmail.com>
2025-05-31 11:54:47 +0300

    Add files via upload

--- index_data/scripts/updates.js
+++ index_data/scripts/updates.js
@@ -1,4 +1,4 @@
-let basicVersion = "1.0.8-v2";
+let basicVersion = "1.0.9-v2";
 function markUpdateReaded() {
   var modal = document.getElementById("updateModal");
   modal.style.display = "none";
@@ -9,7 +9,10 @@ function updateTextLangs() {
   var updateContent = document.getElementById("updateNoticeContent");
   if(localStorage.getItem("lang") === "en") {
     updateTitle.innerHTML = "What new in Version " + basicVersion;
-    updateContent.innerHTML = "<h3>[30/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML = "<h3>[31/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML += "Some fixes." + "<br>";
+    updateContent.innerHTML += "<hr>";
+    updateContent.innerHTML += "<h3>[30/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "Some fixes." + "<br>";
     updateContent.innerHTML += "<hr>";
     updateContent.innerHTML += "<h3>[27/05/2025]</h3>" + "<br>";
@@ -22,13 +25,13 @@ function updateTextLangs() {
     updateContent.innerHTML += "Added direction of snake walk in pause." + "<br>";
     updateContent.innerHTML += "Added Automatic saving of the score, in case of accidental exit." + "<br>";
     updateContent.innerHTML += "Some fixes." + "<br>";
-    updateContent.innerHTML += "<hr>";
-    updateContent.innerHTML += "<h3>[16/05/2025]</h3>" + "<br>";
-    updateContent.innerHTML += "Some fixes." + "<br>";
   }else
   if(localStorage.getItem("lang") === "ru") {
     updateTitle.innerHTML = "Что нового в версии " + basicVersion;
-    updateContent.innerHTML = "<h3>[30/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML = "<h3>[31/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML += "Некоторые исправления." + "<br>";
+    updateContent.innerHTML += "<hr>";
+    updateContent.innerHTML += "<h3>[30/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "Некоторые исправления." + "<br>";
     updateContent.innerHTML += "<hr>";
     updateContent.innerHTML += "<h3>[27/05/2025]</h3>" + "<br>";
@@ -41,13 +44,13 @@ function updateTextLangs() {
     updateContent.innerHTML += "Добавлено направление змеиного шага в паузе." + "<br>";
     updateContent.innerHTML += "Добавлено автоматическое сохранение счета, в случае случайного выхода." + "<br>";
     updateContent.innerHTML += "Некоторые исправления." + "<br>";
-    updateContent.innerHTML += "<hr>";
-    updateContent.innerHTML += "<h3>[16/05/2025]</h3>" + "<br>";
-    updateContent.innerHTML += "Некоторые исправления." + "<br>";
   }else
   if(localStorage.getItem("lang") === "jp") {
     updateTitle.innerHTML = "バージョン " + basicVersion + " の新機能";
-    updateContent.innerHTML = "<h3>[30/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML = "<h3>[31/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML += "いくつかの修正." + "<br>";
+    updateContent.innerHTML += "<hr>";
+    updateContent.innerHTML += "<h3>[30/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "いくつかの修正." + "<br>";
     updateContent.innerHTML += "<hr>";
     updateContent.innerHTML += "<h3>[27/05/2025]</h3>" + "<br>";
@@ -60,13 +63,13 @@ function updateTextLangs() {
     updateContent.innerHTML += "一時停止中にスネークウォークの方向を変更できるようにしました。" + "<br>";
     updateContent.innerHTML += "誤ってゲームを終了した場合に備えて、スコアの自動保存機能を追加しました。" + "<br>";
     updateContent.innerHTML += "いくつかの修正." + "<br>";
-    updateContent.innerHTML += "<hr>";
-    updateContent.innerHTML += "<h3>[16/05/2025]</h3>" + "<br>";
-    updateContent.innerHTML += "いくつかの修正." + "<br>";
   }else
   if(localStorage.getItem("lang") === "chs") {
     updateTitle.innerHTML = basicVersion + " 版新增功能";
-    updateContent.innerHTML = "<h3>[30/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML = "<h3>[31/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML += "一些修复." + "<br>";
+    updateContent.innerHTML += "<hr>";
+    updateContent.innerHTML += "<h3>[30/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "一些修复." + "<br>";
     updateContent.innerHTML += "<hr>";
     updateContent.innerHTML += "<h3>[27/05/2025]</h3>" + "<br>";
@@ -79,12 +82,12 @@ function updateTextLangs() {
     updateContent.innerHTML += "新增暂停时蛇行方向。" + "<br>";
     updateContent.innerHTML += "新增自动保存分数功能，以防意外退出。" + "<br>";
     updateContent.innerHTML += "一些修复." + "<br>";
-    updateContent.innerHTML += "<hr>";
-    updateContent.innerHTML += "<h3>[16/05/2025]</h3>" + "<br>";
-    updateContent.innerHTML += "一些修复." + "<br>";
   }else
   if(localStorage.getItem("lang") === "cht") {
     updateTitle.innerHTML = basicVersion + " 版新增功能";
+    updateContent.innerHTML = "<h3>[31/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML += "一些修復." + "<br>";
+    updateContent.innerHTML += "<hr>";
     updateContent.innerHTML = "<h3>[30/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "一些修復." + "<br>";
     updateContent.innerHTML += "<hr>";
@@ -98,13 +101,13 @@ function updateTextLangs() {
     updateContent.innerHTML += "增加了蛇在暫停時行走的方向。" + "<br>";
     updateContent.innerHTML += "增加了自動儲存分數的功能，以防意外退出。" + "<br>";
     updateContent.innerHTML += "一些修復." + "<br>";
-    updateContent.innerHTML += "<hr>";
-    updateContent.innerHTML += "<h3>[16/05/2025]</h3>" + "<br>";
-    updateContent.innerHTML += "一些修復." + "<br>";
   }else
   if(localStorage.getItem("lang") === "ko") {
     updateTitle.innerHTML = "버전" + basicVersion + "의 새로운 기능";
-    updateContent.innerHTML = "<h3>[30/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML = "<h3>[31/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML += "일부 수정 사항." + "<br>";
+    updateContent.innerHTML += "<hr>";
+    updateContent.innerHTML += "<h3>[30/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "일부 수정 사항." + "<br>";
     updateContent.innerHTML += "<hr>";
     updateContent.innerHTML += "<h3>[27/05/2025]</h3>" + "<br>";
@@ -117,9 +120,6 @@ function updateTextLangs() {
     updateContent.innerHTML += "일시 정지 시 뱀의 걸음 방향을 추가했습니다." + "<br>";
     updateContent.innerHTML += "실수로 게임을 종료할 경우 점수를 자동으로 저장하는 기능을 추가했습니다." + "<br>";
     updateContent.innerHTML += "일부 수정 사항." + "<br>";
-    updateContent.innerHTML += "<hr>";
-    updateContent.innerHTML += "<h3>[16/05/2025]</h3>" + "<br>";
-    updateContent.innerHTML += "일부 수정 사항." + "<br>";
   }else
   if(localStorage.getItem("lang") === "he") {
     updateTitle.innerHTML = "מה חדש בגרסה " + basicVersion;
@@ -128,7 +128,10 @@ function updateTextLangs() {
     updateTitle.style.width = "75%";
     updateContent.style.direction = "rtl";
     updateContent.style.textAlign = "right";
-    updateContent.innerHTML = "<h3>[30/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML = "<h3>[31/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML += "כמה תיקונים." + "<br>";
+    updateContent.innerHTML += "<hr>";
+    updateContent.innerHTML += "<h3>[30/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "כמה תיקונים." + "<br>";
     updateContent.innerHTML += "<hr>";
     updateContent.innerHTML += "<h3>[27/05/2025]</h3>" + "<br>";
@@ -140,13 +143,13 @@ function updateTextLangs() {
     updateContent.innerHTML += "<h3>[20/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "נוסף כיוון של הליכה של נחש בהשהיה." + "<br>";
     updateContent.innerHTML += "נוספה שמירה אוטומטית של הניקוד, במקרה של יציאה מקרית." + "<br>";
-    updateContent.innerHTML += "<hr>";
-    updateContent.innerHTML += "<h3>[16/05/2025]</h3>" + "<br>";
-    updateContent.innerHTML += "כמה תיקונים." + "<br>";
   }else
   if(localStorage.getItem("lang") === "de") {
     updateTitle.innerHTML = "Was ist neu in Version " + basicVersion;
-    updateContent.innerHTML = "<h3>[30/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML = "<h3>[31/05/2025]</h3>" + "<br>";
+    updateContent.innerHTML += "Einige Korrekturen." + "<br>";
+    updateContent.innerHTML += "<hr>";
+    updateContent.innerHTML += "<h3>[30/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "Einige Korrekturen." + "<br>";
     updateContent.innerHTML += "<hr>";
     updateContent.innerHTML += "<h3>[27/05/2025]</h3>" + "<br>";
@@ -158,8 +161,5 @@ function updateTextLangs() {
     updateContent.innerHTML += "<h3>[20/05/2025]</h3>" + "<br>";
     updateContent.innerHTML += "Die Richtung des Schlangengangs wurde in der Pause hinzugefügt." + "<br>";
     updateContent.innerHTML += "Der Spielstand wird nun automatisch gespeichert, falls das Spiel versehentlich beendet wird." + "<br>";
-    updateContent.innerHTML += "<hr>";
-    updateContent.innerHTML += "<h3>[16/05/2025]</h3>" + "<br>";
-    updateContent.innerHTML += "Einige Korrekturen." + "<br>";
   }
 }
