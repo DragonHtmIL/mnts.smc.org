e50b3d51087c7294fb8b25c467e477650638b446
DragonHtmIL <meirfons1@gmail.com>
2026-03-05 14:18:11 +0200

    Add files via upload

--- gacha_events/events_register.js
+++ gacha_events/events_register.js
@@ -2,9 +2,17 @@ function registerGachaEvents() {
   const gachaEventsRegContainer = document.getElementById("scriptsCreatesRegistrationEvents");
   const shadowEvent = document.createElement("script");
   const cosmicechoEvent = document.createElement("script");
-  shadowEvent.src = "index_data/gacha_events/shadow/load.js";
-  cosmicechoEvent.src = "index_data/gacha_events/cosmic_echo/load.js";
+  shadowEvent.src = "https://dragonhtmil.github.io/mnts.smc.org/gacha_events/shadow/load.js";
+  cosmicechoEvent.src = "https://dragonhtmil.github.io/mnts.smc.org/gacha_events/cosmic_echo/load.js";
   gachaEventsRegContainer.appendChild(shadowEvent);
   gachaEventsRegContainer.appendChild(cosmicechoEvent);
+  window.addEventListener('online', function() {
+    gachaEventsRegContainer.appendChild(shadowEvent);
+    gachaEventsRegContainer.appendChild(cosmicechoEvent);
+  });
+  window.addEventListener('offline', function() {
+    gachaEventsRegContainer.removeChild(shadowEvent);
+    gachaEventsRegContainer.removeChild(cosmicechoEvent);
+  });
 }
 registerGachaEvents();
